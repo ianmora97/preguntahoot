878dcb46f57eb93034416f414026b7bff5e6b42e
Ian <ianmorar03@gmail.com>
2020-07-06 04:38:39 -0600

    spring boot

---
+++ Proyect/src/presentation/RegistroApplication.java
@@ -0,0 +1,9 @@
+package presentation;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RegistroApplication extends Application{
+    
+}
